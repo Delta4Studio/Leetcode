7232f4b62cebc5be451ae244cf265646463ab0c8
山田かみら <superfishman9@gmail.com>
2015-10-10 22:42:16 +0800

    Create 118.py

---
+++ 118 Pascal's Triangle/118.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def generate(self, numRows):
+        if numRows == 0:
+            return []
+        x=[[1]]
+        if numRows == 1:
+            return x
+        for i in range(1, numRows):
+            x.append([])
+            x[i].append(1)
+            for j in range(1, i):
+                x[i].append(x[i][j-1] * (i+1-j) / j )
+            x[i].append(1)
+        return x
