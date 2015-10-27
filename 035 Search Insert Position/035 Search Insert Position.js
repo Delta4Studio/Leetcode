88316199faf1cbc3a14386d5abf7e0ea2694e1b4
kamira <superfishman9@gmail.com>
2015-10-27 22:55:32 +0800

    35th problem solved

---
+++ 035 Search Insert Position/035 Search Insert Position.js
@@ -0,0 +1,19 @@
+var searchInsert = function(nums, target){
+    if(nums[0] >= target)
+        return 0;
+    var l=0, r=nums.length, m=parseInt((l+r)/2+0.5);
+    if(target > nums[-1])
+        return r
+    if(target === nums[r-1])
+        return r-1;
+    while(true){
+        if(target <= nums[m] && target > nums[m-1])
+            return m;
+        else if(target < nums[m])
+            r = m;
+        else if(target > nums[m])
+            l = m;
+        m = parseInt((l+r)/2+0.5);
+    }
+};
+//112ms
