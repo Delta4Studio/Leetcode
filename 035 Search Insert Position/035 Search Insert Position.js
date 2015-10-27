fcf6674e56f96d9ef9c60cf728935b8c23962bff
kamira <superfishman9@gmail.com>
2015-10-27 23:00:57 +0800

    bug fixed

--- 035 Search Insert Position/035 Search Insert Position.js
+++ 035 Search Insert Position/035 Search Insert Position.js
@@ -2,7 +2,7 @@ var searchInsert = function(nums, target){
     if(nums[0] >= target)
         return 0;
     var l=0, r=nums.length, m=parseInt((l+r)/2+0.5);
-    if(target > nums[-1])
+    if(target > nums[r-1])
         return r
     if(target === nums[r-1])
         return r-1;
